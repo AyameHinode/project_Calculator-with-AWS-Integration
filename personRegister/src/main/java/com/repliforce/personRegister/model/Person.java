ad216aa8648e141252933a0538783c558c72d976
Ayame Hinode <ayamehinode@gmail.com>
2023-10-23 00:01:44 -0300

    Person entity implemented

--- personRegister/src/main/java/com/repliforce/personRegister/model/Person.java
+++ personRegister/src/main/java/com/repliforce/personRegister/model/Person.java
@@ -3,18 +3,31 @@ package com.repliforce.personRegister.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+
+    @Column(nullable = false, length = 100)
     private String address;
+
+    @Column(nullable = false, length = 10)
     private String gender;
 
     public Person(){}
---
+++ personRegister/src/main/java/com/repliforce/personRegister/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.repliforce.personRegister.repositories;
+
+import com.repliforce.personRegister.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}

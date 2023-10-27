63f212044a37cbf35475771a119b56acddee7dca
Ayame Hinode <ayamehinode@gmail.com>
2023-10-27 20:46:39 -0300

    Including Dozer tests

--- personRegister/src/main/java/com/repliforce/personRegister/repositories/PersonRepository.java
+++ personRegister/src/main/java/com/repliforce/personRegister/repositories/PersonRepository.java
@@ -1,9 +1,10 @@
 package com.repliforce.personRegister.repositories;
 
+import com.repliforce.personRegister.data.vo.v1.PersonVO;
 import com.repliforce.personRegister.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
+public interface PersonRepository extends JpaRepository<PersonVO, Long> {
 }
